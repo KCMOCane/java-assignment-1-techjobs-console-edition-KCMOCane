8a51f8f8b1d9179a8509a26754feff9c5ac504f0
Corey Taylor <kcmocane@gmail.com>
2022-04-03 19:05:32 -0500

    complete assignment

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -59,7 +59,7 @@ public class TechJobs {
 
                 // What is their search term?
                 System.out.println("\nSearch term:");
-                String searchTerm = in.nextLine();
+                String searchTerm = in.nextLine().toLowerCase();
 
                 if (searchField.equals("all")) {
                     printJobs(JobData.findByValue(searchTerm));
@@ -120,6 +120,18 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()){
+            System.out.print("No Results");
+        } else {
+
+            for (int i = 0; i<someJobs.size(); i++) {
+                System.out.println("\n" + "*****");
+                for (Map.Entry<String, String> jobs : someJobs.get(i).entrySet()) {
+                    System.out.println(jobs.getKey()+ ": " + jobs.getValue());
+
+                }
+                System.out.print("*****" + "\n");
+            }
+        }
     }
 }
